104df247209dadee9a335dc7355274f87465658f
GenericJM <juan.arturo.m.m@gmail.com>
2023-08-30 07:59:09 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Button from './assets/components/Button'
+function App() {
+  return <main>
+    <section>      
+      <Button/>
+   </section>
+ </main>
+}
+
+export default App
